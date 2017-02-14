0e3ef43c3952823b53b43193dc81302ed9799bab
Seethalakshmi <seethalakshmi@Systems-MacBook-Pro-2.local>
2017-02-14 19:31:40 +0530

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name selfLearningApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the selfLearningApp
+ */
+angular.module('selfLearningApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
